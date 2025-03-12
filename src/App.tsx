5f296846fabf3c93d0c3404b32fb777cc3236709
Marcelle Yanari <yanarimy@gmail.com>
2025-03-12 17:24:00 -0300

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { Router } from "./Router";
+import { GlobalStyle } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <main>hello</main>
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
