ec36735fe1def5fa362c743a5cb11a78ee9d9e79
Marcelle Yanari <yanarimy@gmail.com>
2025-03-12 19:06:59 -0300

    header, icons

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { Scroll, Timer } from "phosphor-react";
+import { HeaderContainer } from "./styles";
+import { NavLink } from "react-router-dom";
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <span>logo</span>
+            <nav>
+                <NavLink to="/" title="Timer">
+                    <Timer size={24} />
+                </NavLink>
+                <NavLink to="/history" title="Histórico">
+                    <Scroll size={24} />
+                </NavLink>
+            </nav>
+        </HeaderContainer>
+    )
+}
